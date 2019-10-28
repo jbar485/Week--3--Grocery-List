dca3c0a3b6f0dbdb75b1ee323607a4820c9724fd
Student <example@example.com>
2019-10-28 14:29:03 -0700

    minor tweaks to split() parameter

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,12 @@
-var groceryList = [];
-var newList = [];
+
 $(document).ready(function(){
   $("#groceryForm").submit(function(event){
-    var groceryInput = $("input#listEntry").val().toUpperCase().split(" ").sort();
-    groceryList.push(groceryInput);
+    var groceryInput = $("input#listEntry").val().toUpperCase().split(",").sort();
     console.log("Grocery List: ", groceryInput)
-
     groceryInput.forEach(function(grocery){
       $("#listItems").append("<li>" + grocery + "</li>");
     });
-
-
-
-    // if(groceryList.includes(",")){
-    //   groceryList.split(",");
-    //   newList.push("groceryList");
-    //   console.log("New List: ", newList);
-    // }
-
-
-    // $(".container").hide();
-  event.preventDefault();
+    $(".container").hide();
+      event.preventDefault();
   });
 });
