0fd963ed33d1398d5750a11f85a26a756b78456a
Student <example@example.com>
2019-10-28 14:15:58 -0700

    add partially functional groceryInput

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,25 @@
 var groceryList = [];
+var newList = [];
 $(document).ready(function(){
   $("#groceryForm").submit(function(event){
-    var groceryInput = $("input#listEntry").val();
+    var groceryInput = $("input#listEntry").val().toUpperCase().split(" ").sort();
     groceryList.push(groceryInput);
     console.log("Grocery List: ", groceryInput)
+
+    groceryList.forEach(function(grocery){
+      $("#listItems").append("<li>" + grocery + "</li>");
+    });
+
+
+
+    // if(groceryList.includes(",")){
+    //   groceryList.split(",");
+    //   newList.push("groceryList");
+    //   console.log("New List: ", newList);
+    // }
+
+
+    // $(".container").hide();
   event.preventDefault();
   });
 });
